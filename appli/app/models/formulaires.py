04650b26becbc1add17e4c233aeea6fdd4509281
fant1-LB <fantinpro84@gmail.com>
2025-03-20 14:53:34 +0100

    attaqué les routes, et surtout le formulaire pour la recherche

--- appli/app/models/formulaires.py
+++ appli/app/models/formulaires.py
@@ -35,7 +35,7 @@ class AjoutUtilisateur(FlaskForm):
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SelectMultipleField, TextAreaField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Optional
 
 class RechercheFestivalMonument(FlaskForm):
     nom = StringField("Nom", validators=[DataRequired(), Length(min=2, max=100)])
@@ -63,3 +63,10 @@ class AjoutUtilisateur(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Mot de passe", validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Ajouter l\'utilisateur')
+#tentative de formulaire multifacette pr trouver un festival
+class recherche(FlaskForm):
+    nom = StringField("Nom du festival", validators=[Optional(), Length(min=2, max =50)])
+    #potentiellement transformer les selectfield qui suivent en radiofield selon comment ça se présente au final
+    periode = SelectField('Période', choices=[('avant','Avant-Saison (1 Janvier-20 Juin)')('saison','Saison (21 Juin-5 Septembre)')('apres','Après-saison (6 septembre - 31 décembre)')] validators=[DataRequired()])
+    discipline = SelectField('Discipline', choices =[('arts_visu','Arts visuels, arts numériques'),("cinema","Cinémna et audiovisuel"), ("livres","Livres et littérature"),("musique","Musique"),("spectacle_vivant","Spectacle vivant"),("autre","Autres")], validators=[Optional()])
+    #réfléchir avant de faire les lieux

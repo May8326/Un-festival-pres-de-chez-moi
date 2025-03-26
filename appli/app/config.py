f05609056ddf19e54c2107c14805b5d6e74030e4
May8326 <maelys.gioan@outlook.fr>
2025-03-26 14:30:50 +0100

    corrige syntaxe jetons wtf

--- appli/app/config.py
+++ appli/app/config.py
@@ -11,4 +11,4 @@ class Config():
     SQLALCHEMY_ECHO=os.environ.get("SQLALCHEMY_ECHO")
     RESOURCES_PER_PAGE = int(os.environ.get("RESOURCES_PER_PAGE"))
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    WTF_CSRF_SECRET_KEY = os.environ.get("WTF_CSRF_SECRET_KEY")
+    WTF_CSRF_SECRET_KEY = os.environ.get("WTF_CSRF_ENABLE")

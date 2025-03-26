2c9b9026b13173d044ac7d6572d4e6987b3da9a0
Baghate <mathieu.taybi@outlook.fr>
2025-03-26 12:13:04 +0100

    aaargh

--- appli/app/app.py
+++ appli/app/app.py
@@ -2,10 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_login import LoginManager
-<<<<<<< HEAD
-
-=======
->>>>>>> Mathieu
 
 app = Flask(__name__, template_folder="templates", static_folder='statics')
 app.config.from_object(Config)

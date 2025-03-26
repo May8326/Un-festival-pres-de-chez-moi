a3168fc7b3bac5a72f71c553a70ac367e240ca23
May8326 <maelys.gioan@outlook.fr>
2025-03-26 17:01:15 +0100

    corrigé ordre des imports

--- appli/app/app.py
+++ appli/app/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from app.config import Config
 from flask_login import LoginManager
+from app.config import Config
 
 app = Flask(__name__, template_folder="templates", static_folder='statics')
 app.config.from_object(Config)
+
+# Créez les instances avant les importations
 db = SQLAlchemy(app)
-from .routes import generales, insertion, users
 login = LoginManager(app)
+
+# Définissez les configurations de login si nécessaire
+login.login_view = 'login'  # assurez-vous que cette route existe
+
+# Importez les routes APRÈS avoir créé db et login
+from .routes import generales, insertion, users

4b8d373706f7dca0c104456171106a991600ddf0
May8326 <maelys.gioan@outlook.fr>
2025-03-27 16:06:34 +0100

    option de modif de l'adresse mail + gestion du compte

--- appli/app/routes/users.py
+++ appli/app/routes/users.py
@@ -1,6 +1,6 @@
 from flask import url_for, render_template, redirect, request, flash
 from ..models.users import Users
-from ..models.formulaires import AjoutUtilisateur, Connexion
+from ..models.formulaires import AjoutUtilisateur, Connexion, SuppressionUtilisateur, ModificationUtilisateur
 from ..utils.transformations import  clean_arg
 from app.app import app
 from flask_login import login_user, logout_user, current_user
@@ -54,5 +54,54 @@ def deconnexion():
         logout_user()
     flash("Vous êtes déconnecté", "info")
     return redirect(url_for("accueil"))
+
+# VOIR SI ON AJOUTE UNE PAGE D'AFFICHAGE DU COMPTE UTILISATEUR
+
+# @app.route("/utilisateurs/mon_compte", methods=["GET"])
+# def mon_compte():
+#     if current_user.is_authenticated is False:
+#         flash("Vous devez être connecté pour accéder à cette page", "warning")
+#         return redirect(url_for("connexion"))
+#     else:
+#         return render_template("pages/mon_compte.html")
+#         flash("Vous êtes sur votre compte", "info")
+#     return render_template("pages/compte.html", form=form)
+
+@app.route("/utilisateurs/modification", methods=["GET", "POST"])
+def modification():
+    form = ModificationUtilisateur()
+
+    if form.validate_on_submit():
+        statut, donnees = Users.modification(
+            prenom=clean_arg(request.form.get("prenom", None)),
+            password=clean_arg(request.form.get("password", None))
+        )
+        if statut is True:
+            flash("Modification effectuée", "success")
+            return redirect(url_for("accueil"))
+        else:
+            flash(",".join(donnees), "error")
+            return render_template("pages/modification_utilisateur.html", form=form)
+    else:
+        return render_template("pages/modification_utilisateur.html", form=form)
+    
+@app.route("/utilisateurs/suppression", methods=["GET", "POST"])
+def suppression():
+    form = SuppressionUtilisateur()
+
+    if form.validate_on_submit():
+        statut, donnees = Users.suppression(
+            prenom=clean_arg(request.form.get("prenom", None)),
+            password=clean_arg(request.form.get("password", None))
+        )
+        if statut is True:
+            flash("Suppression effectuée", "success")
+            return redirect(url_for("accueil"))
+        else:
+            flash(",".join(donnees), "error")
+            return render_template("pages/suppression_utilisateur.html", form=form)
+    else:
+        return render_template("pages/suppression_utilisateur.html", form=form)
+
 
 login.login_view = 'connexion'

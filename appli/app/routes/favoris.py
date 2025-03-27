a00aba123f548a3a3161fceaefb0879ade0d2148
May8326 <maelys.gioan@outlook.fr>
2025-03-27 15:53:35 +0100

    ajouté l'email au compte utilisateur

--- appli/app/routes/favoris.py
+++ appli/app/routes/favoris.py
@@ -114,57 +114,6 @@ def liste_favoris():
     ).all()
     return render_template("pages/liste_favoris.html", sous_titre="Liste des Favoris", favoris=favoris)
 
-@app.route("/modification/favori", methods=['GET', 'POST'])
-@login_required
-def modification_favori():
-    form = ModificationFavori()
-    form.type.choices = [('festival', 'Festival'), ('monument', 'Monument')]
-
-    try:
-        if form.validate_on_submit():
-            user_id = current_user.id
-            ancien_nom = form.ancien_nom.data
-            nouveau_nom = form.nouveau_nom.data
-            type = form.type.data
-
-            favori = None
-            if type == 'festival':
-                festival = Festival.query.filter_by(nom_festival=ancien_nom).first()
-                if festival:
-                    favori = db.session.query.filter_by(user_id=user_id, festival_id=festival.id_festival).first()
-                    nouveau_festival = Festival.query.filter_by(nom_festival=nouveau_nom).first()
-                    if favori and nouveau_festival:
-                        favori.festival_id = nouveau_festival.id_festival
-                    else:
-                        flash("Nouveau festival non trouvé", 'error')
-                        return render_template("pages/modification_favori.html", sous_titre="Modification Favori", form=form)
-                else:
-                    flash("Ancien festival non trouvé", 'error')
-                    return render_template("pages/modification_favori.html", sous_titre="Modification Favori", form=form)
-            elif type == 'monument':
-                monument = MonumentHistorique.query.filter_by(nom_monument=ancien_nom).first()
-                if monument:
-                    favori = db.session.query.filter_by(user_id=user_id, monument_id=monument.id_monument_historique).first()
-                    nouveau_monument = MonumentHistorique.query.filter_by(nom_monument=nouveau_nom).first()
-                    if favori and nouveau_monument:
-                        favori.monument_id = nouveau_monument.id_monument_historique
-                    else:
-                        flash("Nouveau monument non trouvé", 'error')
-                        return render_template("pages/modification_favori.html", sous_titre="Modification Favori", form=form)
-                else:
-                    flash("Ancien monument non trouvé", 'error')
-                    return render_template("pages/modification_favori.html", sous_titre="Modification Favori", form=form)
-
-            db.session.commit()
-
-            flash("Le favori a été modifié avec succès", 'info')
-    
-    except Exception as e:
-        flash("Une erreur s'est produite lors de la modification du favori : " + str(e), "error")
-        db.session.rollback()
-    
-    return render_template("pages/modification_favori.html", sous_titre="Modification Favori", form=form)
-
 @app.route("/suppression/favori", methods=['GET', 'POST'])
 @login_required
 def suppression_favori():

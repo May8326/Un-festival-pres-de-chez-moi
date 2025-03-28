7833793788db8dc1de16f102b0ed4a5e95dddddf
May8326 <maelys.gioan@outlook.fr>
2025-03-28 23:57:15 +0100

    corrigé config

--- appli/app/config.py
+++ appli/app/config.py
@@ -10,5 +10,6 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO= os.environ.get("SQLALCHEMY_ECHO")
     RESULTATS_PER_PAGE= int(os.environ.get("RESULTATS_PER_PAGE"))
-    SECRET_KEY = os.environ.get("SECRET_KEY", "votre_cle_secrete")  # Ajoutez une clé par défaut si elle n'est pas définie
-    WTF_CSRF_ENABLED = True  # Assurez-vous que CSRF est activé
+    SECRET_KEY = os.environ.get("SECRET_KEY")  # Ajoutez une clé par défaut si elle n'est pas définie
+    WTF_CSRF_ENABLED = os.environ.get("WTF_CSRF_ENABLED")
+    WTF_CSRF_SECRET_KEY = os.environ.get("WTF_CSRF_SECRET_KEY")
